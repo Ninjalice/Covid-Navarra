2263a025350e07ce30f0d07c59e2a8005177e01d
Ninjalice <2012mineral@gmail.com>
2020-11-01 14:45:45 +0100

    V0.1

--- keys_format.py
+++
@@ -1,9 +0,0 @@
-
-# keys_format.py shares the same format as keys.py.
-# This file is not meant to be used in the main script.
-# Instead, copy this file into keys.py and put your own
-# keys there.
-CONSUMER_KEY = 'fcW7WnfL3JTBt1vFs4kzwgX7e'
-CONSUMER_SECRET = 'jmf67qMb4P8bkeZCKNW8c9MsUztABesxqGNlAhgXz8zYskbwJG'
-ACCESS_KEY = '915675941178892288-K5cXxKtLGBEmoZlVaIlYeJaSFG0KyeC'
-ACCESS_SECRET = 'YSlEKSKIRrxU5ZpT1k5ZOzEFYkcUhegZVL5zhooIwgP4d'
